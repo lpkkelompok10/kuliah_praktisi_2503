56fb1576abaf6d040e56aa2ef24a23cec225df4c
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-05-23 10:31:09 +0700

    Add reset button and greeting functionality

--- app.py
+++ app.py
@@ -16,3 +16,9 @@ st.write("Hello, *World!* :sunglasses:")
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
