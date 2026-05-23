451fa403c1356a78a997cc585a41e6a30f31f08a
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-05-23 10:41:05 +0700

    Add image columns for cat, dog, and owl
    Added columns to display images of a cat, dog, and owl.

--- app.py
+++ app.py
@@ -22,3 +22,17 @@ if st.button("Say hello"):
     st.write("Why hello there")
 else:
     st.write("Goodbye")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
