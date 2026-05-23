a909e5252d552f9a567e382aad59f8b73e94b4b0
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-05-23 10:13:49 +0700

    Update Streamlit page configuration settings

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-# app.py — baris 13
 st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide"
+  page_title="kuliah praktisi 2026",
+  layout="centered"
+  page_icon="🧊"
 )
+
 
 # Hirarki teks
 st.title("📊 Dashboard")
