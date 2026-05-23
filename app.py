a94d5a7362c5b73aa16c0d3ccf4ceae209d730e9
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-05-23 10:49:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,16 @@ st.write("Hello, *World!* :sunglasses:")
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
 
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+
 st.button("Reset", type="primary")
 if st.button("Say hello"):
     st.write("Why hello there")
