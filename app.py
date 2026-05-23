31fbce2e84f71af159bc1efb40839e62cb04704f
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-05-23 10:16:38 +0700

    Update page title for Streamlit app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title="kuliah praktisi 2026",
-  layout="centered"
+  page_title="kuliah praktisi 2305",
+  layout="centered",
   page_icon="🧊"
 )
 
